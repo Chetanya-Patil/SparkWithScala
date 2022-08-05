cd7243ffc3f8d506c3650c998c70e26768bdcf40
Chetanya <chaitanyapatil853@gmail.com>
2022-08-05 23:14:54 +0530

    Conversion

---
+++ src/main/scala/com/SparkLOWConstruct/Example12_ConvertRDDToDataframes.scala
@@ -0,0 +1,41 @@
+package com.SparkLOWConstruct
+
+import org.apache.spark.sql.SparkSession
+
+object Example12_ConvertRDDToDataframes {
+  def main(args:Array[String]):Unit = {
+
+    val spark = SparkSession.builder()
+      .appName("Convert rdd to dataframe")
+      .master("local[2]")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    val columns = Seq("language","users_count")
+    val data = Seq(("Java",2000),("Python",100000),("Scala",200000))
+
+    val rdd = spark.sparkContext.parallelize(data)
+
+    val dfFromRDD1 = rdd.toDF()
+    dfFromRDD1.printSchema()
+
+    // By default, toDF() function creates column names as “_1” and “_2” like Tuples.
+
+    // toDF() has another signature that takes arguments to define columns.
+
+    val dfFromRDD2 = rdd.toDF("language","users_counts")
+    dfFromRDD2.printSchema()
+
+
+    // Convert RDD to DataFrame - Using createDataFrame()
+
+    val dfFromRDD3 = spark.createDataFrame(rdd).toDF(columns:_*)
+    dfFromRDD3.printSchema()
+    dfFromRDD3.show(false)
+
+    // Convert RDD to Dataset
+
+  }
+
+}
